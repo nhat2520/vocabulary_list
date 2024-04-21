7e8efe46aaace7a750b054ad21a497216e90cd18
Adviphk <kimdunghk200504@gmail.com>
2024-04-21 22:08:33 +0700

    update route readpdf, youtubesubtitle from Than

--- server/APIs/download_mp3_file.js
+++ server/APIs/download_mp3_file.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const path = require("path");
 
 class YoutubeDownloader {
     constructor() {}
@@ -7,12 +8,10 @@ class YoutubeDownloader {
     download(videoId) {
         return new Promise((resolve, reject) => {
             const stream = ytdl(`http://www.youtube.com/watch?v=${videoId}`, { filter: 'audioonly' })
-                .pipe(fs.createWriteStream(`./mp3_downloaded/${videoId}.mp3`));
-
+                .pipe(fs.createWriteStream(path.join(__dirname + `/../APIs/mp3_downloaded/${videoId}.mp3`)));
             stream.on('finish', () => resolve(`./mp3_downloaded/${videoId}.mp3`));
             stream.on('error', reject);
         });
     }
 }
-
 module.exports = YoutubeDownloader;
